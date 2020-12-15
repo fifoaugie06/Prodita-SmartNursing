e57a7211e07f1bfdabd4a10395493d91ffb1fb63
Augie Winanda Favourite <fifoaugie06@gmail.com>
2020-12-15 10:15:07 +0700

    Biar keliatan ngepush

--- lib/dashboard/components/spacebar.dart
+++ lib/dashboard/components/spacebar.dart
@@ -1,95 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class Spacebar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    DateTime now = DateTime.now();
-    String formattedDate = DateFormat('HH').format(now);
-
-    return FlexibleSpaceBar(
-      collapseMode: CollapseMode.parallax,
-      background: Container(
-        padding: EdgeInsets.only(left: 16, right: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Smart Nursing',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600),
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: TabBar(
+          indicatorColor: Color(0xFFFF4C58),
+          tabs: [
+            Tab(
+              text: "DROP RATE",
             ),
-            Container(
-              margin: EdgeInsets.only(top: 22.0),
-              child: Text(
-                'Keep healthy and have a nice day!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400),
-              ),
+            Tab(
+              text: "TIME OVER",
             ),
-            Container(
-              margin: EdgeInsets.only(top: 12.0),
-              child: Padding(
-                padding: const EdgeInsets.only(right: 30),
-                child: Text(
-                  'Prodita Smart Nursing merupakan aplikasi yang didesain untuk '
-                      'membantu dalam perhitungan jumlah tetesan infus per menit.',
-                  style: TextStyle(color: Color(0xFF7A77B0), fontSize: 14),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.start,
-            //   children: <Widget>[
-            //     RaisedButton(
-            //       shape: RoundedRectangleBorder(
-            //         borderRadius: BorderRadius.circular(16.0),
-            //       ),
-            //       onPressed: () {
-            //
-            //       },
-            //       color: Color(0xFFFF4C58),
-            //       textColor: Colors.white,
-            //       child: Padding(
-            //         padding: const EdgeInsets.only(top: 8, bottom: 8),
-            //         child: Text(
-            //           makeGreetingFromNow(formattedDate),
-            //           style: TextStyle(fontSize: 13),
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // ),
           ],
         ),
       ),
     );
-  }
-
-  String makeGreetingFromNow(String formattedDate) {
-    String greeting = "Selamat ";
-    int format = int.parse(formattedDate);
-
-    if (format >= 0 && format <= 10) {
-      greeting += "Pagi";
-    } else if (format > 10 && format <= 14) {
-      greeting += "Siang";
-    } else if (format > 14 && format <= 18) {
-      greeting += "Sore";
-    } else {
-      greeting += "Malam";
-    }
-
-    return greeting;
   }
 }
--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -10,63 +10,17 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      // drawer: Theme(
-      //   data: Theme.of(context).copyWith(canvasColor: Color(0xff2A2A2A)),
-      //   child: NavDrawer(),
-      // ),
-      body: DefaultTabController(
-        length: 2,
-        child: NestedScrollView(
-          headerSliverBuilder: (context, value) {
-            return [
-              SliverAppBar(
-                expandedHeight: 280.0,
-                floating: true,
-                pinned: true,
-                snap: false,
-                elevation: 50,
-                backgroundColor: Color(0xFF473E97),
-                flexibleSpace: Spacebar(),
-                bottom: PreferredSize(
-                  preferredSize: Size.fromHeight(100.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFF473E97),
-                      border: Border(
-                        top: BorderSide(
-                          color: Color(0xFF7A77B0),
-                        ),
-                      ),
-                    ),
-                    child: TabBar(
-                      indicatorColor: Color(0xFFFF4C58),
-                      tabs: [
-                        Tab(
-                          text: "DROP RATE",
-                        ),
-                        Tab(
-                          text: "TIME OVER",
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ];
-          },
-          body: TabBarView(
-            children: [
-              DropRate(),
-              TimeOver(),
-            ],
+      body: Column(
+        children: [
+          Container(
+            height: 230,
+            color: Colors.red,
           ),
-        ),
+          Spacebar(),
+        ],
       ),
     );
   }
